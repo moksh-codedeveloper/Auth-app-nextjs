870da53ad001265030a02526ce143e875384a1d8
Moksh Malde <maldemoksh437@gmail.com>
2025-02-21 16:27:42 +0530

    Add login and signup pages with basic structure

---
+++ src/app/login/page.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-center text-white text-2xl">Login</h1>
+        </div>
+    );
+}
